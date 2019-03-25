39a6a5d2df9919680277fa0b28af051eb3625f92
deannabosschert <deanna.bosschert@gmail.com>
2019-03-25 16:14:33 +0100

    insert name of task and category into one array

--- index.js
+++ index.js
@@ -2,49 +2,57 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const port = 3000
 const task = ['']
 const complete = ['']
-const port = 3000
-const listName = ['']
-const category = ['']
-const taskWhole = task + category
+// const taskWhole = task + category
 
 app.use(bodyParser.urlencoded({ extended: true }))
 .set('view engine', 'ejs')
 .use(express.static('public'))
 .post('/addtask', addTask)
-.post('/removetask', moveTaskToComplete)
+.post('/completeTask', moveTask)
 .get('/', shoppingList)
 .listen(3000, function(){
   console.log(`Example app listening on port ${port}!`)})
 
 function shoppingList(req, res) {
-    res.render('index', { task: task, category: category, complete: complete, listName: listName})
+    res.render('index', { task: task})
 }
 
 function addTask(req, res) {
     const newTask = req.body.newtask
-    task.push(newTask)
+    // task.push(newTask)
     const newCategory = req.body.category
-    category.push(newCategory)
+    // category.push(newCategory)
+    task.push({
+      name: newTask,
+      category: newCategory,
+      checked: false
+    })
+    // [['milk', 'diary'], ['lettuce', 'vegetables']]
+    // [{task: 'milk', category: 'diary'}, {}]
     res.redirect('/')
 }
 
-function moveTaskToComplete(req, res) {
-    const completeTask = req.body.check
-    //check for the 'typeof' the different completed task, then add into the complete task
-    if (typeof completeTask === 'string') {
-        complete.push(completeTask)
-        //check if the completed task already exists in the task when checked, then remove it
-        task.splice(task.indexOf(completeTask), 1)
-    } else if (typeof completeTask === 'object') {
-        for (const i = 0; i < completeTask.length; i++) {
-            complete.push(completeTask[i]);
-            task.splice(task.indexOf(completeTask[i]), 1)
-        }
-    }
-    res.redirect('/')
+function moveTask(req, res){
+  res.redirect('/')
 }
+// function moveTaskToComplete(req, res) {
+//     const completeTask = req.body.check
+//     //check for the 'typeof' the different completed task, then add into the complete task
+//     if (typeof completeTask === 'string') {
+//         complete.push(completeTask)
+//         //check if the completed task already exists in the task when checked, then remove it
+//         task.splice(task.indexOf(completeTask), 1)
+//     } else if (typeof completeTask === 'object') {
+//         for (const i = 0; i < completeTask.length; i++) {
+//             complete.push(completeTask[i]);
+//             task.splice(task.indexOf(completeTask[i]), 1)
+//         }
+//     }
+//     res.redirect('/')
+// }
 
 // function moveTaskToComplete(req, res) {
 //     const completeTask = req.body.task
